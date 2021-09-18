fe515f854a5f8cb5c47246fd49bea13045d46935
BitDEVil2K16 <pcroot1@gmail.com>
2021-09-18 06:37:55 +0200

    Copyright in Anja Add Links to Github on LandingPage

--- classes/AnjaScript.php
+++ classes/AnjaScript.php
@@ -1,5 +1,15 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
+/*
+ * Copyright (c) 2021. BitDEVil2K16 Club. All rights reserved.
+ * @author BitDEVil2K16 (Sascha P.)
+ * @author BitDEVil2K16 Club <support@pc-dev.info>
+ * @author BitDEVil2K16 Club https://bitdevil2k16.club
+ * @github https://github.com/BitDEVil2K16
+ * @FileName: AnjaScript.php
+ *
+ * Special Thanks to Anja as namesake!!
+ *
+ */
 class AnjaScript{
     private $_test = "Nice";
     public $test;
--- pages/home.php
+++ pages/home.php
@@ -1,8 +1,28 @@
-
+<style>
+    .h3{
+        font-size: 24px;
+    }
+    a{
+        text-decoration: none;
+        color: #0a6ebd;
+    }
+    a:visited{
+        color: #0a6ebd;
+    }
+</style>
 <h3 class="centerflex">
     <div>Hallo hier werden wir dan als Nächstes mal ein Template einbinden damit wir ein wenig Frontend Haben!</div>
     <div id="hiddenpart" class="hiddeblock">
         <h1><span style="color: lime">I was Hidden!</span></h1>
+    </div>
+</h3>
+<hr />
+<h3 class="centerflex">
+    <div class="h3">
+        ToDo und so : <a href="https://github.com/BitDEVil2K16/PHP_Class_Framework/projects/1" target="_blank">Github Project</a>
+    </div>
+    <div class="h3">
+        Source Code : <a href="https://github.com/BitDEVil2K16/PHP_Class_Framework" target="_blank">Github</a>
     </div>
 </h3>
 
